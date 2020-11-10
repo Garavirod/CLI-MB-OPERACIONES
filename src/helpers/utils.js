77dec7d01e3bbe0862abf4e9b777824a3a30fb03
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-11-10 12:15:37 -0600

    Se modifico el algortmo  de agrupación

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -99,8 +99,7 @@ export const setKilometrajeCalculado = (referencia) =>{
     Esta funcion recibe un conjunto de registros entre
     cumplientos e incmplimientos.
 
-    Retorna los registros agrupados por fechas y 
-    kilometraje total acumulado en esa fecha.
+    Retorna los registros agrupados por fechas
 */
 
 
@@ -108,25 +107,25 @@ export const GruopedDataByDate = (data, afectacion) =>{
     let values = [];
     let dateRef = data[0].fecha;
     let collections = [data[0]];
-    let km = data[0].Cumplimiento_Incumplimientos[0].kilometraje;
+    // let km = data[0].Cumplimiento_Incumplimientos[0].kilometraje;
     console.log(data);
     for (let idx = 1; idx < data.length; idx++) {
         if(data[idx].fecha === dateRef){      
             collections.push(data[idx]);                 
-            if(afectacion && data[idx].Cumplimiento_Incumplimientos.length>1){
-                let km1 = data[idx].Cumplimiento_Incumplimientos[0].kilometraje; 
-                let km2 = data[idx].Cumplimiento_Incumplimientos[1].kilometraje; 
-                km =  Math.abs(km2-km1);                
+            // if(afectacion && data[idx].Cumplimiento_Incumplimientos.length>1){
+            //     let km1 = data[idx].Cumplimiento_Incumplimientos[0].kilometraje; 
+            //     let km2 = data[idx].Cumplimiento_Incumplimientos[1].kilometraje; 
+            //     km =  Math.abs(km2-km1);                
 
-            }else{
-                km += data[idx].Cumplimiento_Incumplimientos[0].kilometraje;      
+            // }else{
+            //     km += data[idx].Cumplimiento_Incumplimientos[0].kilometraje;      
 
-            }
+            // }
             if(idx===data.length-1){
                 const obj = {
                     date: dateRef,
                     collection : collections,
-                    kmtotal: parseFloat((Math.abs(km)).toFixed(3))
+                    // kmtotal: parseFloat((Math.abs(km)).toFixed(3))
                 }
                 values.push(obj);
             }               
@@ -135,24 +134,24 @@ export const GruopedDataByDate = (data, afectacion) =>{
             const obj = {
                 date: dateRef,
                 collection : collections,
-                kmtotal: parseFloat((Math.abs(km)).toFixed(3))
+                // kmtotal: parseFloat((Math.abs(km)).toFixed(3))
             }
             values.push(obj);
             dateRef = data[idx].fecha;
             collections = [data[idx]];
-            km = data[idx].Cumplimiento_Incumplimientos[0].kilometraje;  
+            // km = data[idx].Cumplimiento_Incumplimientos[0].kilometraje;  
             if(idx===data.length-1){
 
-                if(afectacion && data[idx].Cumplimiento_Incumplimientos.length>1){
-                    let km1 = data[idx].Cumplimiento_Incumplimientos[0].kilometraje; 
-                    let km2 = data[idx].Cumplimiento_Incumplimientos[1].kilometraje; 
-                    km =  Math.abs(km2-km1);                
-                }
+                // if(afectacion && data[idx].Cumplimiento_Incumplimientos.length>1){
+                //     let km1 = data[idx].Cumplimiento_Incumplimientos[0].kilometraje; 
+                //     let km2 = data[idx].Cumplimiento_Incumplimientos[1].kilometraje; 
+                //     km =  Math.abs(km2-km1);                
+                // }
 
                 const obj = {
                     date: dateRef,
                     collection : collections,
-                    kmtotal: parseFloat((Math.abs(km)).toFixed(3))
+                    // kmtotal: parseFloat((Math.abs(km)).toFixed(3))
                 }
                 values.push(obj);
             }
